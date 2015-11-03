47876e4f1635bc83ccc1e909c265b42f16243fda
Tran Kien <Kien Tran>
2015-11-03 11:43:59 +0700

    no message

---
+++ CODataImport/CODataImport/DataImport.h
@@ -0,0 +1,31 @@
+//
+//  DataImport.h
+//  DataImport
+//
+//  Created by Tran Kien on 10/1/15.
+//  Copyright (c) 2015 Tran Kien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataImport.
+FOUNDATION_EXPORT double DataImportVersionNumber;
+
+//! Project version string for DataImport.
+FOUNDATION_EXPORT const unsigned char DataImportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataImport/PublicHeader.h>
+
+#import <CoreData/CoreData.h>
+#import <MagicalRecord/MagicalRecord.h>
+
+#define kIS_SHOW_ERROR NO
+
+
+#ifdef CLSNSLog
+  #define GGCDLOG(__FORMAT__, ...) CLSNSLog((@"%s line %d $ " __FORMAT__), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#define GGCDLOG(__FORMAT__, ...) 
+#endif
+
+
