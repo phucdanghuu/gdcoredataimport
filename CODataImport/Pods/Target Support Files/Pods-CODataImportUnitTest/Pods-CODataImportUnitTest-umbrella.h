6fdbb7b8533be3752d6ca6604661c45e439093c2
Tran Kien <kien.tran@cogini.com>
2016-10-16 14:05:22 +0700

    - Fixed unit test

---
+++ CODataImport/Pods/Target Support Files/Pods-CODataImportUnitTest/Pods-CODataImportUnitTest-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_CODataImportUnitTestVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CODataImportUnitTestVersionString[];
+
