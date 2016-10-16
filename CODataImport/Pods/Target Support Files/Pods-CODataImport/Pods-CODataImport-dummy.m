4b917308e37298271afdf5d978bccba9285bd4fc
Tran Kien <kien.tran@cogini.com>
2016-10-16 19:49:38 +0700

    - Removed Pod folder

--- CODataImport/Pods/Target Support Files/Pods-CODataImport/Pods-CODataImport-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CODataImport : NSObject
-@end
-@implementation PodsDummy_Pods_CODataImport
-@end
--- CODataImport/Pods/Target Support Files/Pods-CODataImportUnitTest/Pods-CODataImportUnitTest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CODataImportUnitTest : NSObject
-@end
-@implementation PodsDummy_Pods_CODataImportUnitTest
-@end
